fb84f0967fe152caf0b95607d31cee46eacf64b4
German Velardez <germanvelardez16@gmail.com>
2023-01-22 19:06:52 -0300

    add readme

--- main.py
+++ main.py
@@ -7,22 +7,43 @@ from selenium.webdriver.common.by import By
 
 PATH = r"~/driver/chormedriver"
 os.environ['PATH']+=PATH
+driver = None
 
 
-def Main():
+
+
+def init_driver():
+    global driver 
+    driver = webdriver.Chrome()
+
+
+
+def find_element():
     try:
-        driver = webdriver.Chrome()
-        driver.get("http://www.python.org")
-        assert "Python" in driver.title
         elem = driver.find_element(By.NAME, "q")
         elem.clear()
         elem.send_keys("pyenv")
         elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source # Comprobamos que tenemos resultados validos diferentes de No "result found"
-      #  driver.close()
+        # Comprobamos que tenemos resultados validos diferentes de No "result found"
+        assert "No results found." not in driver.page_source 
+    except :
+        pass
+
+def get_page(page):
+    global driver
+    driver.get(page)
+
+def close_driver():
+    global driver
+    driver.close()
+
+def Main():
+    try:
+        init_driver()
+        get_page("http://www.python.org")
         while True:
             pass   # Si el programa termina la ventana chrom se cierra rapidamente
-
+        close_driver()
         
        
     except:
